4e998ad003acd6c95aef2b56f84755c05b0fd09f
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-05-19 15:12:25 +0200

    Stampa elementi nel documento

--- index.php
+++ index.php
@@ -1,5 +1,44 @@
 <?php
 
+// array $hotels
+    $hotels = [
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+    ];
+
 ?>
 
 <!DOCTYPE html>
@@ -8,9 +47,65 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
     <title>Php Hotel</title>
 </head>
 <body>
     
+    <h1>Bella zio</h1>
+
+    <ul>
+        <?php
+
+            foreach($hotels as $elem) {
+
+                if ($elem['parking'] == true) {
+
+                    echo 
+                    '<div class="card" style="width: 18rem;">
+                        <div class="card-body"> <h5 class="card-title">'. $elem['name'] .'</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">
+                                Distance to center: '. $elem['distance_to_center'] .
+                            '</h6>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">
+                                Vote: '. $elem['vote'] .
+                            '</h6>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">
+                                Parking: Yes
+                            </h6>
+                            <p class="card-text">'. $elem['description'] .'</p>
+                        </div> 
+                    </div>';
+                    
+                } else {
+
+                    echo 
+                    '<div class="card" style="width: 18rem;">
+                        <div class="card-body"> <h5 class="card-title">'. $elem['name'] .'</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">
+                                Distance to center: '. $elem['distance_to_center'] .
+                            '</h6>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">
+                                Vote: '. $elem['vote'] .
+                            '</h6>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">
+                                Parking: No
+                            </h6>
+                            <p class="card-text">'. $elem['description'] .'</p>
+                        </div> 
+                    </div>';
+                }
+
+            }
+        ?>
+    </ul>
+
+    
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
